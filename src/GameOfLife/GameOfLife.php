f34cfab62eeec3f2c22ba1dcf39ae5fbe2cc316b
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-04-26 19:09:40 +0200

    [GREEN] Return 'dead' if living in underpopulation

--- src/GameOfLife/GameOfLife.php
+++ src/GameOfLife/GameOfLife.php
@@ -7,5 +7,11 @@ namespace GameOfLife;
  */
 class GameOfLife
 {
+    public function getNextStatus($currentStatus, $aliveNeighbours)
+    {
+        if ($currentStatus === 'alive' && $aliveNeighbours < 2)
+        {
+            return 'dead';
+        }
+    }
 }
-
